e1d45700eabb2cefb1925cdc6f17dd079c12792c
Naveen_Kumar_Singh <naveen_kumar_singh@epam.com>
2023-03-21 21:02:43 +0530

    Initial commit

---
+++ src/test/java/springrestsample/springrestsample/SpringRestSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package springrestsample.springrestsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
